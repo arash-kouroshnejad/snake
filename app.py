0f49ffec82cc1c7dde23cf08ed9c75f16e44dc75
arash <kooroshnezhad2004@gmail.com>
2022-08-31 08:41:44 -0700

    login page complete

--- app.py
+++ app.py
@@ -7,17 +7,28 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# configure database of users
-db = SQL("sqlite3:///users.db")
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
-@app.route("/game")
+# configure database of users
+db = SQL("sqlite:///users.db")
+
+@app.route("/")
 def game():
     # ensurre the user is logged in
-    if session["user_id"] == None:
+    if session.get("user_id") is None:
         return redirect("/login")
     
     # 
@@ -26,7 +37,7 @@ def game():
 def login():
     # GET
     if request.method == "GET":
-        return render_template("/login")
+        return render_template("/login.html")
 
     # POST
     if request.method("POST"):
@@ -40,11 +51,9 @@ def login():
             return Response("{'error':'username'}",status=400,mimetype='application/json')
         
         # check for invalid password
-        if not check_password_hash(rows[0]["hash"],password):
+        if not (check_password_hash(rows[0]["hash"],password) and password):
             return Response("{'error':'password'}",status=400)
 
         session["user_id"] = rows[0]["id"]
         return redirect("/game")
 
-
-def register():
