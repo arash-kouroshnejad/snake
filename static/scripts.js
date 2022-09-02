02f98bbc2044293a0d87cbc01e708ab48c80ace0
arash <kooroshnezhad2004@gmail.com>
2022-09-02 07:16:12 -0700

    ??

--- static/scripts.js
+++ static/scripts.js
@@ -1,22 +1,42 @@
 snake1 = document.getElementById("snake1")
 snake2 = document.getElementById("snake2")
 snake3 = document.getElementById("snake3")
-snake1.addEventListener("click",function () {
+gameScreen = document.getElementById("gameScreen")
+function selectSnake(el) {
+    let ajax = new XMLHttpRequest;
+    ajax.onreadystatechange = function () {
+        // preparing snake preference
+        let data = {"snake":el.id}
+        data = JSON.stringify(data)
+        if ((ajax.readyState==4)&&(ajax.status==200)) {
+            // snake preference saved
+            // remove modal
+            modal();
 
-});
+            // load resources
+            gameScreen.innerHTML = ajax.responseText;
 
-// save snake preference
+            // play 
+            play();
+        }
+    }
+}
+snake1.addEventListener("click",selectSnake(event.srcElement));
+snake2.addEventListener("click",selectSnake(event.srcElement));
+snake3.addEventListener("click",selectSnake(event.srcElement));
+
+// play
+function play() {
 
 
 
-
-
-// load game 
+} 
 
 
 
 
 // remove modal 
+function modal() {
 
+}
 
-// load resources
